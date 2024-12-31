1178ffb97a7da5f48ebf7004ba5e8a7c3186ccb2
Igor Katkov <ikatkov@gmail.com>
2024-12-31 11:40:22 -0800

    build: fix manifest again

--- update-manifest.js
+++ update-manifest.js
@@ -19,7 +19,7 @@ try {
 }
 
 // Update the start_url field with the new version
-manifest.start_url = `/html-rf-convert/?version=${version}`;
+manifest.start_url = `/html-rf-convert/dbconvert.html?version=${version}`;
 
 // Write the updated manifest.json back to the file
 try {
